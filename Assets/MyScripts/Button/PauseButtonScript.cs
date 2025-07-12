66cbafd5a29e4439be9110f954a5b72cb201f87c
valeriew1 <pronina.lera2006@gmail.com>
2025-07-12 12:22:18 +0300

    добавились кнопочки поставить на паузу по спэйсу и продолжить игру
    прыжок все еще зависит от скорости

--- Assets/MyScripts/Button/PauseButtonScript.cs
+++ Assets/MyScripts/Button/PauseButtonScript.cs
@@ -7,11 +7,15 @@ public class PauseButtonScript : MonoBehaviour
 {
 
     [SerializeField] Button PauseButton;
+    //GameManager gameManager;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        PauseButton.onClick.AddListener(PauseCanvasAppear);
+        //PauseButton.onClick.AddListener(PauseCanvasAppear);
+        //gameManager.PauseGame();
+        InputManager.Instance.OnPausePressed += PauseCanvasAppear;
     }
 
     // Update is called once per frame
@@ -23,12 +27,17 @@ public class PauseButtonScript : MonoBehaviour
 
     public void PauseCanvasAppear()
     {
+        //gameManager.PauseGame();
+        //GameManager.ChangeState(PauseGame());
+        //GameManager.ChangeState(GameManager.Instance.PauseGame());
+        //GameManager.Instance.PauseGame();
         Canvas[] allCanvases = Resources.FindObjectsOfTypeAll<Canvas>();
         foreach (Canvas canvas in allCanvases)
         {
             if (canvas.name == "PauseCanvas")
             {
                 canvas.gameObject.SetActive(true);
+                GameManager.Instance.PauseGame();
 
             }
             if (canvas.name == "onLevelCanvas") 
@@ -39,10 +48,10 @@ public class PauseButtonScript : MonoBehaviour
         }
     }
 
-
-    public void OnMouseOver()
-    {
+   
+    //public void OnMouseOver()
+    //{
            
-    }
+    //}
 
 }
