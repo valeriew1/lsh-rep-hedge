b868ad03b9d1f8b5cc5a2517b8b4c3f4cf1aafc9
valeriew1 <pronina.lera2006@gmail.com>
2025-07-15 01:52:52 +0300

    исправлено для билда, работает

--- Assets/MyScripts/BackGround/BackGroundCreateScript.cs
+++ Assets/MyScripts/BackGround/BackGroundCreateScript.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
-using static UnityEditor.Progress;
 using Random = UnityEngine.Random;
 
 public class BackGroundCreateScript : MonoBehaviour
