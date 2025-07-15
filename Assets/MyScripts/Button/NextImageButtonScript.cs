14086c7878f10ef5fd598dccbaa0286929657e69
valeriew1 <pronina.lera2006@gmail.com>
2025-07-15 11:03:41 +0300

    с добавлением кнопочек на обучение

--- Assets/MyScripts/Button/NextImageButtonScript.cs
+++ Assets/MyScripts/Button/NextImageButtonScript.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NextImageButtonScript : MonoBehaviour
 {
+    [SerializeField] Button nextImageButton;
+    [SerializeField] GameObject[] images;
+    [SerializeField] private int MaxIndex;
+    private int Index = 0;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        nextImageButton.onClick.AddListener(OnNextImageButton);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnNextImageButton() 
     {
-        
+        if (Index + 1 <= MaxIndex)
+        {
+            images[Index].SetActive(false);
+            images[Index + 1].SetActive(true);
+            Index++;
+        }
     }
+
 }
